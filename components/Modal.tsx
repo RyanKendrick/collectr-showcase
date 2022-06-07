525591c6633c0fb3a8afe4d7728e35e88f669bb9
Adam Hijleh <aahijleh@gmail.com>
2022-06-07 12:53:14 -0400

    increase width/height of expanded images + prettier

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,36 +1,39 @@
-import React, { FunctionComponent } from 'react'
-import Image from 'next/image'
+import React, { FunctionComponent } from "react";
+import Image from "next/image";
 
 interface ModalProps {
-    selectedImage: string;
-    closeModal: any;
+  selectedImage: string;
+  closeModal: any;
 }
 
-const Modal: FunctionComponent<ModalProps> = ({ selectedImage, closeModal }) => {
+const Modal: FunctionComponent<ModalProps> = ({
+  selectedImage,
+  closeModal,
+}) => {
   return (
     <>
-        <div className='modal' id='modal' onClick={closeModal}>
-            <div>
-                <Image
-                    src={selectedImage ? selectedImage : '/loading.gif'}
-                    width={250}
-                    height={300}
-                    alt={'Expanded view'}
-                    className={'product-image'}
-                    objectFit={'contain'}
-                />
-                <div className='close'>
-                    <Image 
-                        src={'/close.svg'}
-                        width={25}
-                        height={25}
-                        alt={'close modal'}
-                    />
-                </div>
-            </div>
+      <div className="modal" id="modal" onClick={closeModal}>
+        <div>
+          <Image
+            src={selectedImage ? selectedImage : "/loading.gif"}
+            width={300}
+            height={350}
+            alt={"Expanded view"}
+            className={"product-image"}
+            objectFit={"contain"}
+          />
+          <div className="close">
+            <Image
+              src={"/close.svg"}
+              width={25}
+              height={25}
+              alt={"close modal"}
+            />
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import type { AppProps } from 'next/app'
-import '../styles/Header.scss'
-import '../styles/globals.scss'
-import '../styles/UserProfile.scss'
-import '../styles/Collection.scss'
-import '../styles/ProductCard.scss'
-import '../styles/CollectionSkeleton.scss'
-import '../styles/home/Header.scss'
-import '../styles/home/Footer.scss'
-import '../styles/home/HeroBanner.scss'
+import type { AppProps } from "next/app";
+import "../styles/Header.scss";
+import "../styles/globals.scss";
+import "../styles/UserProfile.scss";
+import "../styles/Collection.scss";
+import "../styles/ProductCard.scss";
+import "../styles/CollectionSkeleton.scss";
+import "../styles/home/Header.scss";
+import "../styles/home/Footer.scss";
+import "../styles/home/HeroBanner.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
