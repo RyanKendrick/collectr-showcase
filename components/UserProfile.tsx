48721159fbb84cdf5eabaca7eb36c1f25d494adb
Adam Hijleh <aahijleh@gmail.com>
2022-06-07 12:53:38 -0400

    prettier+ remove logging

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent } from 'react'
-import Image from 'next/image'
-import CircularProgress from '@mui/material/CircularProgress'
+import React, { FunctionComponent } from "react";
+import Image from "next/image";
+import CircularProgress from "@mui/material/CircularProgress";
 
 interface UserInfoProps {
   userAvatar: string;
@@ -12,74 +12,82 @@ interface UserInfoProps {
   collectorBadge: any;
 }
 
-
-const UserProfile: FunctionComponent<UserInfoProps> = ({ userAvatar, userName, portfolioValue, totalCards, totalSealed, totalGraded, collectorBadge }) => {
+const UserProfile: FunctionComponent<UserInfoProps> = ({
+  userAvatar,
+  userName,
+  portfolioValue,
+  totalCards,
+  totalSealed,
+  totalGraded,
+  collectorBadge,
+}) => {
   const setBadge = async () => {
-
     if (parseFloat(totalSealed) > parseFloat(totalCards)) {
-      collectorBadge = 'https://www.getcollectr.com/public-assets/images/sealed-collectr-icon.png'
+      collectorBadge =
+        "https://www.getcollectr.com/public-assets/images/sealed-collectr-icon.png";
     } else if (parseFloat(totalGraded) / parseFloat(totalCards) > 0.5) {
-      collectorBadge ='https://www.getcollectr.com/public-assets/images/graded-collectr-icon.png'
+      collectorBadge =
+        "https://www.getcollectr.com/public-assets/images/graded-collectr-icon.png";
     } else {
-      collectorBadge = 'https://www.getcollectr.com/public-assets/images/raw-cards-collectr-icon.png'
+      collectorBadge =
+        "https://www.getcollectr.com/public-assets/images/raw-cards-collectr-icon.png";
     }
-    console.log('totalSealed', totalSealed)
-  }
-  setBadge()
-  
+  };
+  setBadge();
+
   return (
     <>
-        {!userAvatar && (
-          <CircularProgress />
-        )}
-        {userAvatar && (
-          <>
-            <div className='badge-icon'>
+      {!userAvatar && <CircularProgress />}
+      {userAvatar && (
+        <>
+          <div className="badge-icon">
             <Image
-                src={collectorBadge ? collectorBadge : '/loading.gif'}
-                width={100}
-                height={100}
-                alt={'collector badge'}
-                className={'collector-badge'}
-              />
-                  </div>
-                  <div className='user-info-container'>
+              src={collectorBadge ? collectorBadge : "/loading.gif"}
+              width={100}
+              height={100}
+              alt={"collector badge"}
+              className={"collector-badge"}
+            />
+          </div>
+          <div className="user-info-container">
             <div className="column">
-                <div>
+              <div>
                 <Image
-                    src={userAvatar ? userAvatar : '/loading.gif'}
-                    alt={'user avatar'}
-                    width={100}
-                    height={100}
-                    className={'user-avatar'}
+                  src={userAvatar ? userAvatar : "/loading.gif"}
+                  alt={"user avatar"}
+                  width={100}
+                  height={100}
+                  className={"user-avatar"}
                 />
               </div>
-              <span className='user-name'>{userName}</span>
+              <span className="user-name">{userName}</span>
               <span>Estimated Portfolio Value (USD)</span>
-              <span className='number'>{portfolioValue}</span>
+              <span className="number">{portfolioValue}</span>
             </div>
             <div className="row">
-              <div className='row-container'>
-                <span>Total Cards</span><br/>
-                <span className='number'>{totalCards}</span>
+              <div className="row-container">
+                <span>Total Cards</span>
+                <br />
+                <span className="number">{totalCards}</span>
               </div>
-              <div className='row-container'>
-                <span>Total Sealed</span><br/>
-                <span className='number'>{totalSealed}</span>
+              <div className="row-container">
+                <span>Total Sealed</span>
+                <br />
+                <span className="number">{totalSealed}</span>
               </div>
-              <div className='row-container'>
-                <span>Total Graded</span><br />
-                <span className='number'>{totalGraded}</span>
+              <div className="row-container">
+                <span>Total Graded</span>
+                <br />
+                <span className="number">{totalGraded}</span>
               </div>
             </div>
-                  </div>
-          </>
-        )}
-        
+          </div>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
 
-// Props: userAvatar, userName, portfolioValue, totalCards, totalSealed, totalGraded 
+// Props: userAvatar, userName, portfolioValue, totalCards, totalSealed, totalGraded
