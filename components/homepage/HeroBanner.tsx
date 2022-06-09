22aa06e4e4911b30c26b8bd2e9a669d65adb2ce3
RyanKendrick <rpkendrick@gmail.com>
2022-06-09 12:24:28 -0400

    heroBanner slider progress

--- components/homepage/HeroBanner.tsx
+++ components/homepage/HeroBanner.tsx
@@ -1,6 +1,15 @@
-import React, { FunctionComponent, useRef, useState, useEffect } from 'react'
+import React, { FunctionComponent } from 'react'
 import Image from 'next/image'
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay, Pagination } from 'swiper'
+SwiperCore.use([Autoplay]);
+SwiperCore.use([Pagination]);
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/bundle'
 
 interface HeroBannerProps {
     images: any;
@@ -10,7 +19,55 @@ const HeroBanner: FunctionComponent<HeroBannerProps> = ({ images }) => {
  
   return (
     <>
+    
     <div className="slideshow">
+      <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            autoplay={true}
+            pagination={{ clickable: true }}
+      
+          >
+      
+            <SwiperSlide>
+
+            <div className='slideshow'>
+              <Image
+                src={'https://www.tributemedia.com/hubfs/Images/Blog%20Images/shutterstock_252081805.jpg'}
+                alt={'banner'}
+                width={800}
+                height={300}
+                className={'current-img'}
+              />
+            </div>
+            </SwiperSlide>
+            <SwiperSlide>
+            <div className='slideshow'>
+              <Image
+                src={'https://www.tributemedia.com/hubfs/Images/Blog%20Images/shutterstock_252081805.jpg'}
+                alt={'banner'}
+                width={800}
+                height={300}
+                className={'current-img'}
+              />
+            </div>
+            </SwiperSlide>
+            <SwiperSlide>
+            <div className='slideshow'>
+              <Image
+                src={'https://www.tributemedia.com/hubfs/Images/Blog%20Images/shutterstock_252081805.jpg'}
+                alt={'banner'}
+                width={800}
+                height={300}
+                className={'current-img'}
+              />
+            </div>
+            </SwiperSlide>
+          </Swiper>
+    </div>
+    {/* <div className="slideshow">
       <div className="cover-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. </div>
       <Image 
         src={'https://www.tributemedia.com/hubfs/Images/Blog%20Images/shutterstock_252081805.jpg'}
@@ -19,7 +76,7 @@ const HeroBanner: FunctionComponent<HeroBannerProps> = ({ images }) => {
         height={300}
         className={'current-img'}
       />
-    </div>
+    </div> */}
         
     </>
   )
