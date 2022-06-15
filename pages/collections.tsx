22708e725571501b0c1e2748e61afdf9d0031bf4
Adam Hijleh <aahijleh@gmail.com>
2022-06-15 15:08:44 -0400

    .

--- pages/collections.tsx
+++ pages/collections.tsx
@@ -1,30 +1,32 @@
-import { NextPage } from 'next'
-import React, { useState, useEffect } from 'react'
-import ShowcaseCard from '../components/homepage/ShowcaseCard'
-import axios from 'axios'
-import Header from '../components/homepage/Header'
-import Footer from '../components/homepage/Footer'
-import { useRouter } from 'next/router'
+import { NextPage } from "next";
+import React, { useState, useEffect } from "react";
+import ShowcaseCard from "../components/homepage/ShowcaseCard";
+import axios from "axios";
+import Header from "../components/homepage/Header";
+import Footer from "../components/homepage/Footer";
+import { useRouter } from "next/router";
 import HtmlHead from "../components/homepage/HtmlHead";
-import Head from '../components/Head'
+import Head from "../components/Head";
 
 const CategoriesPage: NextPage = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    const { query: {categoryId}} = router
+  const {
+    query: { categoryId },
+  } = router;
 
-    const props = { categoryId }
+  const props = { categoryId };
 
-    type results = {
-        product_name: any;
-        image_url: any;
-        categories: any;
-    };
-    type categoriesList = {
-        any: any;
-    }
+  type results = {
+    product_name: any;
+    image_url: any;
+    categories: any;
+  };
+  type categoriesList = {
+    any: any;
+  };
 
-    console.log('props.categoryId', props.categoryId)
+  //   console.log("props.categoryId", props.categoryId);
 
   const [results, setResults] = useState<results[]>([]);
   const [category, setCategory] = useState(
@@ -32,9 +34,9 @@ const CategoriesPage: NextPage = () => {
   );
   const [categoriesList, setCategoriesList] = useState<categoriesList[]>([]);
   const [rOffset, setROffset] = useState(0);
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   // let resultsOffset = 0
-  let resultsLimit = 28;
+  let resultsLimit = 36;
 
   const getData = () => {
     axios
@@ -44,77 +46,96 @@ const CategoriesPage: NextPage = () => {
       .then((showcases) => {
         setResults(showcases.data.data);
         setCategories();
-        setROffset(rOffset + 28);
+        setROffset(rOffset + 36);
       });
   };
 
-    const getCategoryData = async (e: any) => {
-        const close: any = document.getElementById('dropdown-content')
-        close.className = 'display-none'
-        setIsOpen(false)
-        document.getElementById('dropdown-content')
-        console.log('etv', e.target.value)
-        setCategory(e.target.value)
-        await axios.get(`https://djk9wkkysj.execute-api.us-east-1.amazonaws.com/data/showcase?filters=${e.target.value}&offset=${rOffset}&limit=${resultsLimit}`)
-        .then((response) => {
-            console.log('response.data', response.data.data)
-            setResults(response.data.data)
-        })
-    }
+  const getCategoryData = async (e: any) => {
+    const close: any = document.getElementById("dropdown-content");
+    close.className = "display-none";
+    setIsOpen(false);
+    document.getElementById("dropdown-content");
+    // console.log("etv", e.target.value);
+    setCategory(e.target.value);
+    await axios
+      .get(
+        `https://djk9wkkysj.execute-api.us-east-1.amazonaws.com/data/showcase?filters=${e.target.value}&offset=${rOffset}&limit=${resultsLimit}`
+      )
+      .then((response) => {
+        // console.log("response.data", response.data.data);
+        setResults(response.data.data);
+      });
+  };
 
-    const setCategories = () => {
-        axios.get(`https://djk9wkkysj.execute-api.us-east-1.amazonaws.com/data/showcase/categories`)
-        .then((response) => {
-            setCategoriesList(response.data.data)
-        })
-    }
+  const setCategories = () => {
+    axios
+      .get(
+        `https://djk9wkkysj.execute-api.us-east-1.amazonaws.com/data/showcase/categories`
+      )
+      .then((response) => {
+        setCategoriesList(response.data.data);
+      });
+  };
 
-    const openDropdown = () => {
-        if (isOpen === false) {
-            const open: any = document.getElementById('dropdown-content')
-            setIsOpen(true)
-            open.className = 'show-dropdown'
-        } else {
-            const close: any = document.getElementById('dropdown-content')
-            close.className = 'display-none'
-            setIsOpen(false)
-        }
+  const openDropdown = () => {
+    if (isOpen === false) {
+      const open: any = document.getElementById("dropdown-content");
+      setIsOpen(true);
+      open.className = "show-dropdown";
+    } else {
+      const close: any = document.getElementById("dropdown-content");
+      close.className = "display-none";
+      setIsOpen(false);
     }
+  };
 
-    useEffect(() => {
-       getData()
+  useEffect(() => {
+    getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    
-    const loadMore = () => {
-        setROffset(rOffset + 20)
-        axios.get(`https://djk9wkkysj.execute-api.us-east-1.amazonaws.com/data/showcase?filters=${category}&offset=${rOffset}&limit=${resultsLimit}`)
-        .then((response) => {
-            setResults((results) => ([...results, ...response.data.data]))
-        }).catch(err => {
-            console.log('error', err)
-        })
-        console.log('results offset', rOffset)
-    }
+  }, []);
+
+  const loadMore = () => {
+    setROffset(rOffset + 36);
+    axios
+      .get(
+        `https://djk9wkkysj.execute-api.us-east-1.amazonaws.com/data/showcase?filters=${category}&offset=${rOffset}&limit=${resultsLimit}`
+      )
+      .then((response) => {
+        setResults((results) => [...results, ...response.data.data]);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+    // console.log("results offset", rOffset);
+  };
 
   return (
     <>
-        <HtmlHead /> 
-        <Head />       
-        <Header />
+      <HtmlHead />
+      <Head />
+      <Header />
 
-        <div className='category-select'>
-            <div className="dropdown">
-                <button onClick={openDropdown} className="dropbtn">Categories ▼</button>
-                <div id='dropdown-content' className="dropdown-content">
-                        {Array.from(new Set(categoriesList)).map((i: any) => (
-                            <button className='dropdown-option' key={i.category_id} onClick={getCategoryData} value={i.category_id}>{i.category_name}</button>
-                        ))}
-                </div>
-            </div>
+      <div className="category-select">
+        <div className="dropdown">
+          <button onClick={openDropdown} className="dropbtn">
+            Categories ▼
+          </button>
+          <div id="dropdown-content" className="dropdown-content">
+            {Array.from(new Set(categoriesList)).map((i: any) => (
+              <button
+                className="dropdown-option"
+                key={i.category_id}
+                onClick={getCategoryData}
+                value={i.category_id}
+              >
+                {i.category_name}
+              </button>
+            ))}
+          </div>
         </div>
-        
-        {/* <div className="category-select">
+      </div>
+
+      {/* <div className="category-select">
             <select onChange={getCategoryData} name="categories">
                 <option className='options-placeholder' value="default">Categories</option>
                 {Array.from(new Set(categoriesList)).map((i: any) => (
@@ -122,50 +143,49 @@ const CategoriesPage: NextPage = () => {
                 ))}
             </select>
         </div> */}
-        
-        <div className="showcases-page-container">
-            {category === '' && (
-                results.map((item: any) => (
-                    <>
-                        <ShowcaseCard
-                            showcases={item}
-                            img1={item.image_1_url}
-                            img2={item.image_2_url}
-                            img3={item.image_3_url}
-                            avatar={item.profile_photo_url}
-                            username={item.display_name}
-                            categories={item.categories}
-                            collectionValue={item.total_value}
-                            refId={item.reference_id}
-                        />
-                    </>
-                ))
-            )}
-            {category !== '' && (
-                results.map((item: any) => (
-                    <>
-                        <ShowcaseCard
-                            showcases={item}
-                            img1={item.image_1_url}
-                            img2={item.image_2_url}
-                            img3={item.image_3_url}
-                            avatar={item.profile_photo_url}
-                            username={item.display_name}
-                            categories={item.categories}
-                            collectionValue={item.total_value}
-                            refId={item.reference_id}
-                        />
-                    </>
-                ))
-            )}
-            
-        </div>
-        
-        <button onClick={loadMore} className='load-more-btn'>Load More</button>
-        
-        <Footer />
-    </>
-  )
-}
 
-export default CategoriesPage
+      <div className="showcases-page-container">
+        {category === "" &&
+          results.map((item: any) => (
+            <>
+              <ShowcaseCard
+                showcases={item}
+                img1={item.image_1_url}
+                img2={item.image_2_url}
+                img3={item.image_3_url}
+                avatar={item.profile_photo_url}
+                username={item.display_name}
+                categories={item.categories}
+                collectionValue={item.total_value}
+                refId={item.reference_id}
+              />
+            </>
+          ))}
+        {category !== "" &&
+          results.map((item: any) => (
+            <>
+              <ShowcaseCard
+                showcases={item}
+                img1={item.image_1_url}
+                img2={item.image_2_url}
+                img3={item.image_3_url}
+                avatar={item.profile_photo_url}
+                username={item.display_name}
+                categories={item.categories}
+                collectionValue={item.total_value}
+                refId={item.reference_id}
+              />
+            </>
+          ))}
+      </div>
+
+      <button onClick={loadMore} className="load-more-btn">
+        Load More
+      </button>
+
+      <Footer />
+    </>
+  );
+};
+
+export default CategoriesPage;
