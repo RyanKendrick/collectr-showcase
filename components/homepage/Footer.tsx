0a30c90a84b6b69e761c1712f1855d857795c9c8
Adam Hijleh <aahijleh@gmail.com>
2022-06-14 20:42:22 -0400

    small changes like sizes, margins and font family

--- components/homepage/Footer.tsx
+++ components/homepage/Footer.tsx
@@ -1,55 +1,59 @@
-import React, { FunctionComponent } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+import React, { FunctionComponent } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer: FunctionComponent = () => {
   return (
     <>
-        <div className="footer">
-          <div className="discord-link">
-            <Link href="">Join our Discord Community</Link>
+      <div className="footer">
+        <div className="discord-link">
+          <Link href="https://discord.gg/PxzhbDzFUt">
+            Join our Discord Community
+          </Link>
+        </div>
+        <div className="footer-bottom">
+          <div className="social-icons">
+            <Image
+              alt="social-icon"
+              src={"https://www.svgrepo.com/show/271189/instagram.svg"}
+              width={15}
+              height={15}
+            />
+            <Image
+              alt="social-icon"
+              src={"https://www.svgrepo.com/show/271189/instagram.svg"}
+              width={15}
+              height={15}
+            />
+            <Image
+              alt="social-icon"
+              src={"https://www.svgrepo.com/show/271189/instagram.svg"}
+              width={15}
+              height={15}
+            />
+            <Image
+              alt="social-icon"
+              src={"https://www.svgrepo.com/show/271189/instagram.svg"}
+              width={15}
+              height={15}
+            />
+            <Image
+              alt="social-icon"
+              src={"https://www.svgrepo.com/show/271189/instagram.svg"}
+              width={15}
+              height={15}
+            />
           </div>
-          <div className="footer-bottom">
-            <div className="social-icons">
-              <Image 
-                alt="social-icon"
-                src={'https://www.svgrepo.com/show/271189/instagram.svg'}
-                width={15}
-                height={15}
-              />
-              <Image 
-                alt="social-icon"
-                src={'https://www.svgrepo.com/show/271189/instagram.svg'}
-                width={15}
-                height={15}
-              />
-              <Image 
-                alt="social-icon"
-                src={'https://www.svgrepo.com/show/271189/instagram.svg'}
-                width={15}
-                height={15}
-              />
-              <Image 
-                alt="social-icon"
-                src={'https://www.svgrepo.com/show/271189/instagram.svg'}
-                width={15}
-                height={15}
-              />
-              <Image 
-                alt="social-icon"
-                src={'https://www.svgrepo.com/show/271189/instagram.svg'}
-                width={15}
-                height={15}
-              />
-            </div>
-            <div className="footer-labels">
-              <div className="footer-title">Collectr</div>
-              <div className="copyright">Copyright © 2022 Collectr. All rights reserved.</div>
+          <div className="footer-labels">
+            <div className="footer-title">contact@getcollectr.com</div>
+            <div className="copyright">
+              Copyright © 2022 Collectr. All rights reserved.
             </div>
           </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
