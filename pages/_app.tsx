3d095a3dce1f4f562a13c603aedb7859ce9cbf39
Adam Hijleh <aahijleh@gmail.com>
2022-06-15 18:04:48 -0400

    fix GA - test 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,19 +8,21 @@ import "../styles/CollectionSkeleton.scss";
 import "../styles/home/Header.scss";
 import "../styles/home/Footer.scss";
 import "../styles/home/HeroBanner.scss";
-import '../styles/home/Showcases.scss'
-import '../styles/home/Categories.scss'
-import '../styles/showcases-page/Showcases-Page.scss'
-import ReactGA from 'react-ga';
+import "../styles/home/Showcases.scss";
+import "../styles/home/Categories.scss";
+import "../styles/showcases-page/Showcases-Page.scss";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  ReactGA.initialize(`${'G-TN54HPHYN1'}`);
+  ReactGA.initialize(`${"G-TN54HPHYN1"}`);
+  useEffect(() => {
+    // Client-side-only code
     ReactGA.pageview(window.location.pathname + window.location.search);
+  });
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
-
-
 
 // https://www.googletagmanager.com/gtag/js?id=G-TN54HPHYN1
