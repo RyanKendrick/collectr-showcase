2de8aba3e1572829cf5ebdfc3896b97b4e7a715e
RyanKendrick <rpkendrick@gmail.com>
2022-06-15 18:25:03 -0400

    adding google analytics for Next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,16 +11,35 @@ import "../styles/home/HeroBanner.scss";
 import "../styles/home/Showcases.scss";
 import "../styles/home/Categories.scss";
 import "../styles/showcases-page/Showcases-Page.scss";
-import ReactGA from "react-ga";
-import { useEffect } from "react";
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  ReactGA.initialize(`${"G-TN54HPHYN1"}`);
-  useEffect(() => {
-    // Client-side-only code
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  });
-  return <Component {...pageProps} />;
+  
+  
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
