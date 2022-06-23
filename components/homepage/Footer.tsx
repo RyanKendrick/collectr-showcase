8eacd1627a52c0499946a82b897913fd6be68ad5
RyanKendrick <rpkendrick@gmail.com>
2022-06-23 15:07:35 -0400

    updating footer

--- components/homepage/Footer.tsx
+++ components/homepage/Footer.tsx
@@ -5,49 +5,38 @@ import Image from "next/image";
 const Footer: FunctionComponent = () => {
   return (
     <>
-      <div className="footer">
-        <div className="discord-link">
-          <Link href="https://discord.gg/PxzhbDzFUt">
-            Join our Discord Community
-          </Link>
+      <div className="footer-container">
+        <div className="footer-content">
+          <div className="footer-logo">
+            <a href="https://www.getcollectr.com">
+              <Image
+                  src={'https://www.getcollectr.com/marketing-website/images/collectr_logo-removebg---copy-p-500.png'}
+                  width={200}
+                  height={40}
+                  alt={'collectr-icon'}
+              />
+            </a>
+          </div>
+          <div className="footer-links">
+            <span>Contact Us</span><br/>
+            <a href="mailto:contact@getcollectr.com">contact@getcollectr.com</a><br/>
+            <a href="http://discord.gg/PxzhbDzFUt">Discord</a><br/>
+            <a href="http://instagram.com/getcollectr">Instagram</a>
+          </div>
+          <div className="footer-links">
+            <span>Legal</span><br/>
+            <a href="https://www.getcollectr.com/marketing-website/terms-conditions.html">Terms &amp; Conditions</a><br/>
+            <a href="https://www.getcollectr.com/marketing-website/privacy-policy.html">Privacy Policy</a><br/>
+          </div>
         </div>
-        <div className="footer-bottom">
-          <div className="social-icons">
-            <a href="https://www.facebook.com/getcollectr" target="_blank" rel="noreferrer" className="icon-bg">
-              <Image
-                alt="social-icon"
-                src={"/facebook.png"}
-                width={30}
-                height={30}
-              />
-            </a>
-            <a href="https://www.instagram.com/getcollectr/" target="_blank" rel="noreferrer" className="icon-bg">
-              <Image
-                alt="social-icon"
-                src={"/instagram.png"}
-                width={30}
-                height={30}
-              />
-            </a>
-            <a href="https://twitter.com/getcollectrapp" target="_blank" rel="noreferrer" className="icon-bg">
-              <Image
-                alt="social-icon"
-                src={"/twitter.png"}
-                width={30}
-                height={30}
-              />
-            </a>
-          </div>
-          <div className="footer-labels">
-            <div className="footer-title">contact@getcollectr.com</div>
-            <div className="copyright">
-              Copyright © 2022 Collectr. All rights reserved.
-            </div>
-          </div>
+        <div className="footer-divider"></div>
+        <div className="footer-copyright">
+          <div>&copy; Collectr. All rights reserved</div>
         </div>
       </div>
     </>
   );
 };
+
 
 export default Footer;
